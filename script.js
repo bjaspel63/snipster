8ad3fb616ee64a8c057a66f8336928c150592fda
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 01:57:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // ===== Supabase Setup =====
-import { supabase } from "./supabase-config.js";
-
-// Use supabase directly
-const { data, error } = await supabase.from("snippets").select("*");
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const SUPABASE_URL = "https://ekgqwgqnhpvucwnuupxk.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZ3F3Z3FuaHB2dWN3bnV1cHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzI4NDgsImV4cCI6MjA3MTU0ODg0OH0.IxtcWi0xT6LD-mDeLf4QmVHSJuFKQIuvEqWkBlWRBEs";
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
 // ===== DOM Elements =====
