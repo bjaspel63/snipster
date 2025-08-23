a62345d54f322b2766adc48589b336f6ab991039
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 01:58:08 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-// supabase-config.js
-
-const SUPABASE_URL = "https://ekgqwgqnhpvucwnuupxk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZ3F3Z3FuaHB2dWN3bnV1cHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzI4NDgsImV4cCI6MjA3MTU0ODg0OH0.IxtcWi0xT6LD-mDeLf4QmVHSJuFKQIuvEqWkBlWRBEs";
-
-// Initialize Supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
