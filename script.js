2771dc9074fc21c3fd79fab35668737bc7b92b29
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 01:49:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // ===== Supabase Setup =====
-const SUPABASE_URL = "https://ekgqwgqnhpvucwnuupxk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZ3F3Z3FuaHB2dWN3bnV1cHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzI4NDgsImV4cCI6MjA3MTU0ODg0OH0.IxtcWi0xT6LD-mDeLf4QmVHSJuFKQIuvEqWkBlWRBEs";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './supabase-config.js';
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 // ===== DOM Elements =====
 const cheatsheetEl = document.getElementById("cheatsheet");
