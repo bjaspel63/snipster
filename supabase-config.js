96cdb8b295f5d9ad03fed1729e38da1242216098
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 01:46:51 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,5 @@
 // supabase-config.js
 
-// Replace these values with your Supabase project info
 const SUPABASE_URL = "https://ekgqwgqnhpvucwnuupxk.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZ3F3Z3FuaHB2dWN3bnV1cHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzI4NDgsImV4cCI6MjA3MTU0ODg0OH0.IxtcWi0xT6LD-mDeLf4QmVHSJuFKQIuvEqWkBlWRBEs";
 
