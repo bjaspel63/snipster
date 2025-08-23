ba486753d816cafc34cb89d5201d99a705379f2a
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 02:07:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const auth = firebase.auth();
 
 const SUPABASE_URL = "https://ekgqwgqnhpvucwnuupxk.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZ3F3Z3FuaHB2dWN3bnV1cHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzI4NDgsImV4cCI6MjA3MTU0ODg0OH0.IxtcWi0xT6LD-mDeLf4QmVHSJuFKQIuvEqWkBlWRBEs";
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 
 // ===== DOM Elements =====
