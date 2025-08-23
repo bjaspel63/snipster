359df041807210a792193ca2548768b1a6582597
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 02:25:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const auth = firebase.auth();
 // ===== Supabase Setup =====
 const SUPABASE_URL = "https://ekgqwgqnhpvucwnuupxk.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZ3F3Z3FuaHB2dWN3bnV1cHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NzI4NDgsImV4cCI6MjA3MTU0ODg0OH0.IxtcWi0xT6LD-mDeLf4QmVHSJuFKQIuvEqWkBlWRBEs";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // ===== DOM Elements =====
 const cheatsheetEl = document.getElementById("cheatsheet");
