feb063beff37219aaef68096ecd58df1bce072a6
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-23 23:38:16 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCYXCxOcDwKyMoVQj0hjSIroPrnpza58T0",
+  authDomain: "snipster-d0d4d.firebaseapp.com",
+  projectId: "snipster-d0d4d",
+  storageBucket: "snipster-d0d4d.firebasestorage.app",
+  messagingSenderId: "776489672682",
+  appId: "1:776489672682:web:9b5db075b2bdcad91c5c93",
+  measurementId: "G-7143MHX2TF"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
