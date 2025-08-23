f806be782090364d140c8c16dc5b342b3c362fd2
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-24 00:23:32 +0700

    Update appwrite-config.js

--- appwrite-config.js
+++ appwrite-config.js
@@ -1,11 +1,13 @@
 // Initialize Appwrite client
 const client = new Appwrite.Client()
+
+client
   .setEndpoint("https://syd.cloud.appwrite.io/v1") // Appwrite endpoint
   .setProject("Y68a9eab60024173932c4");                   // Project ID
 
 // Services
 const account = new Appwrite.Account(client);
-const database = new Appwrite.Database(client);
+const databases = new Appwrite.Databases(client);
 
 // Collection ID
 const COLLECTION_ID = "user_snippets";
